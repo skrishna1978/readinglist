79430e672da71d10cb89a647ad38587610c5cdce
Shashi Krishna <skrishna@asd.edu.qa>
2019-10-15 16:34:11 +0300

    Updates

---
+++ precache-manifest.6de220fc25a85bffaafc39316b5244cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf99abccc27943b969c3efac1a171940",
+    "url": "/readinglist/index.html"
+  },
+  {
+    "revision": "e4f2dc82de28cb9f58dd",
+    "url": "/readinglist/static/css/main.1ac6787f.chunk.css"
+  },
+  {
+    "revision": "51d2e5dc22042b33c033",
+    "url": "/readinglist/static/js/2.094b0939.chunk.js"
+  },
+  {
+    "revision": "e4f2dc82de28cb9f58dd",
+    "url": "/readinglist/static/js/main.c1aac5b8.chunk.js"
+  },
+  {
+    "revision": "52843ce4e788802f168f",
+    "url": "/readinglist/static/js/runtime-main.54c665da.js"
+  },
+  {
+    "revision": "febbbccf47c9d6653f0a58c8ef5fd07f",
+    "url": "/readinglist/static/media/logo.febbbccf.png"
+  }
+]);
