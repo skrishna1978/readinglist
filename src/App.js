94e149d26d3787061c9cc9b2e8a7456f2a1a8ca5
shashikrishna1978 <39548480+shashikrishna1978@users.noreply.github.com>
2019-10-15 16:27:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './logo.png';
 import './App.css';
+import Tabletop from 'tabletop';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1rDbZqPYNaJZDmznIQCXyxbb55Jf6ppJrDZhHY_pdzng',
+      callback: googleData => {
+        this.setState({
+          data: googleData
+        })
+      },
+      simpleSheet: true
+    })
+  }
+
+  render() {
+    const { data } = this.state
+    return (
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title"><font color="black">i.(am).read(ing)</font></h1>
+        </header>
+         <div id="book-details">
+          {
+            data.map(obj => {
+              return (
+                     <div key={obj.Title}>
+                         <h1><a href = {obj.Reviews}  target="_blank"> {obj.Title}</a></h1>
+                         <b>{obj.Author}</b>
+                        <p>{obj.Year}, {obj.Country}</p>
+                        <p>{obj.Start} - {obj.End}</p>
+                         <img alt={obj.Cover} src={obj.Cover} height="300" width="200"/>
+                      <hr class="new1"></hr>
+                      </div>
+             )
+            })
+          }
+        </div>
+
+      </div>
+
+    );
+  }
 }
 
 export default App;
